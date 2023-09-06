fb9c94181431e49d8f8c0809311f74f9282c4bc9
Panorami <loud.music.n@gmail.com>
2023-09-06 11:41:35 -0400

    scrollbar
    - resize
    - sp and tb version

--- src/pages/Home/RightNow.js
+++ src/pages/Home/RightNow.js
@@ -1,3 +1,4 @@
+import { useState, useRef, useEffect } from 'react';
 import '../../assets/css/pages/home/rightNow/rightNow.scss';
 import { ReactComponent as ArrowIcon } from '../../assets/images/arrow-icon.svg';
 import Headline from '../../components/atoms/Headline';
@@ -6,6 +7,25 @@ import { data } from '../../data/rightNow/rightNow'
 import MainSection from '../../components/templates/MainSection';
 
 function RightNow() {
+	const [ valueNow, setValueNow ] = useState(0);
+	const indicatorBarElement = useRef();
+	const scrollElement = useRef();
+
+	useEffect(() => {
+		function calculateWidth() {
+			const element = scrollElement.current;
+			const margin = window.innerWidth < 900 ? (element.scrollLeft + element.children[0].getBoundingClientRect().left) : 0;
+			const indicatorWidth = (element.clientWidth - margin) / (element.scrollWidth - margin) * 100;
+			indicatorBarElement.current.style.width = indicatorWidth + '%';
+		}
+
+		calculateWidth()
+		window.addEventListener("resize", calculateWidth)
+		return () => {
+			window.removeEventListener("resize", calculateWidth)
+		}
+	}, [])
+	
 	const listItems = data.map(( item, i ) => {
 		const modifier = 'rightNow__inner--' + item.theme;
 		return (
@@ -39,25 +59,76 @@ function RightNow() {
 		)
 	})
 
+	const handleScroll = (e) => {
+		const element = scrollElement.current;
+		const margin = window.innerWidth < 900 ? (element.scrollLeft + element.children[0].getBoundingClientRect().left) : 0;
+		const clientWidth = element.clientWidth - margin * 2;
+		const scrollWidth = element.scrollWidth - margin;
+
+		// aria-valuenow
+		const maxScrollWidth = scrollWidth - clientWidth;
+		const currentScrollPosition = e.target.scrollLeft;
+		const scrollRatio = currentScrollPosition / maxScrollWidth * 100; 
+		setValueNow(Math.floor(scrollRatio));
+
+		// scroll bar position
+		const y = clientWidth - clientWidth * (clientWidth / scrollWidth);
+		const x = y / indicatorBarElement.current.offsetWidth;
+		const d = x * scrollRatio;
+		indicatorBarElement.current.style.transform = `translateX(${d}%)`;
+	}
+
+	const handleMoveScroll = (e) => {
+		const element = scrollElement.current;
+		const parentMargin = window.innerWidth < 900 ? (element.scrollLeft + element.children[0].getBoundingClientRect().left) : 0;
+		const clientWidth = element.clientWidth - parentMargin * 2;
+
+		const listElement = scrollElement.current.children[0];
+		const getItemRect = (i) => { return listElement.children[i].getBoundingClientRect()};
+		const margin = getItemRect(1).left - getItemRect(0).left - getItemRect(0).width;
+		const itemWidth = getItemRect(0).width + margin;
+		const countDisplayedSlides = Math.floor(clientWidth / itemWidth);
+
+		let left = 0;
+		if(e.key === 'ArrowLeft') {
+			left = scrollElement.current.scrollLeft - itemWidth * countDisplayedSlides;
+		} else if(e.key === 'ArrowRight') {
+			left = scrollElement.current.scrollLeft + itemWidth * countDisplayedSlides;
+		}
+
+		if(e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+			scrollElement.current.scrollTo({
+				left: left, 
+				behavior: 'smooth',
+			});
+		}
+	}
+
 	const contents = <>
 		<Headline headline='Right Now at IKEA' />
 		<div className='rightNow__cardWrapper'>
-			<button aria-controls='carousel'></button>
-			<div className='rightNow__cardInner'>
-				<ul className='rightNow__list' id='carousel'>
+			<button aria-controls='scroll'></button>
+			<div className='rightNow__cardInner' onScroll={ handleScroll } ref={ scrollElement }>
+				<ul className='rightNow__list' id='scroll'>
 					{	listItems }
 				</ul>
 			</div>
-			<button aria-controls='carousel'></button>
+			<button aria-controls='scroll'></button>
 		</div>
 		<div
 			className='rightNow__indicator'
 			role='scrollbar'
-			aria-controls='carousel'
+			aria-controls='scroll'
+			aria-orientation='horizontal'
 			aria-valuemin='0'
-			aria-valuenow='0'
+			aria-valuenow={ valueNow }
 			aria-valuemax='100'
+			tabIndex={0}
+			onKeyDown={ handleMoveScroll }
 			>
+			<span className='rightNow__indicatorWrapper'>
+				<span className='rightNow__indicatorBar' ref={ indicatorBarElement }></span>
+			</span>
 		</div>
 	</> 
 
